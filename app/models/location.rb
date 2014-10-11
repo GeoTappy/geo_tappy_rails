fa5d5210391394bd1f59845e1c318258d0724c94
Konrad Oleksiuk <konrad@up-next.com>
2014-10-11 16:12:29 +0200

    Remove ATMs from Google types of places

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  GOOGLE_TYPES = %w[accounting airport amusement_park aquarium art_gallery atm bakery
+  GOOGLE_TYPES = %w[accounting airport amusement_park aquarium art_gallery bakery
 bank bar beauty_salon bicycle_store book_store bowling_alley bus_station cafe
 campground car_dealer car_rental car_repair car_wash casino cemetery church city_hall
 clothing_store convenience_store courthouse dentist department_store doctor electrician
